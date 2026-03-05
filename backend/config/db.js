702593d478728bee27ca3161a8447e11f2e3193c
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-03-05 23:08:52 +0530

    updated-for-db.query

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,13 @@
 import {Pool} from 'pg';
 import {config} from 'dotenv';
 import {drizzle} from 'drizzle-orm/node-postgres';
+import * as schema from '../models/workspace.model.js'
 config()
 
 const pool = new Pool({
     connectionString:process.env.PostgreSQL_URI
 })
 
-export const db = drizzle(pool);
+export const db = drizzle(pool,{schema});
 
 export default pool;
