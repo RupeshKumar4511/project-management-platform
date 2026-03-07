3de3643a91d2755fd5532d1dd9b2746ba5fdcc99
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-03-07 20:50:15 +0530

    workspace-routes updated

--- backend/routes/workspace.routes.js
+++ backend/routes/workspace.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createWorkspace,joinWorkspace,createProject, createTask, addTeamMemberToWorkspace, addTeamMemberToProject, addComments, getFullWorkspaceDetails, updateWorkspace, updateProject, updateTask, updateProjectMember, deleteWorkspace, deleteProject, deleteTask, deleteComments, deleteProjectMember, deleteTeamMember } from '../controllers/workspace.controller.js';
+import { createWorkspace,joinWorkspace,createProject, createTask, addTeamMemberToWorkspace, addMemberToProject, addComments, getFullWorkspaceDetails, updateWorkspace, updateProject, updateTask, updateProjectMember, deleteWorkspace, deleteProject, deleteTask, deleteComments, deleteProjectMember, deleteWorkspaceMember, getMyTasks } from '../controllers/workspace.controller.js';
 import { checkSchema } from 'express-validator';
 import { workspaceSchema,joinWorkspaceSchema,createProjectSchema, createTaskSchema, addTeamMemberToWorkspaceSchema, updateProjectMemberSchema, addCommentSchema, updateWorkspaceSchema, updateProjectSchema, updateTaskSchema } from '../utils/workspace.validationSchema.js';
 import {schemaValidation} from '../middleware/schema.validation.js'
@@ -11,22 +11,23 @@ const router = express.Router();
 
 router.route('/create-workspace').post(checkSchema(workspaceSchema),schemaValidation,authentication,createWorkspace)
 router.route('/join-workspace').post(checkSchema(joinWorkspaceSchema),schemaValidation,authentication,ensureWorkspaceUser,joinWorkspace);
-router.route('/create-project').post(checkSchema(createProjectSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["admin"]),createProject)
-router.route('/create-task').post(checkSchema(createTaskSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["admin"]),createTask)
-router.route('/add-workspace-member').post(checkSchema(addTeamMemberToWorkspaceSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["admin"]),addTeamMemberToWorkspace)
-router.route('/add-project-member').post(checkSchema(updateProjectMemberSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["admin","member"]),addTeamMemberToProject)
-router.route('/add-comment').post(checkSchema(addCommentSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["admin","member"]),addComments)
-router.route('/get-workspace').get(authentication,ensureWorkspaceUser,restrictTo(['admin',"member"]),getFullWorkspaceDetails)
-router.route('/update-workspace').put(checkSchema(updateWorkspaceSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["admin",]),updateWorkspace)
-router.route('/update-project').put(checkSchema(updateProjectSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["admin","member"]),updateProject)
-router.route('/update-task').put(checkSchema(updateTaskSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["admin","member"]),updateTask)
-router.route('/update-project-member').put(checkSchema(updateProjectMemberSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["admin",]),updateProjectMember)
-router.route('/delete-workspace').delete(authentication,ensureWorkspaceUser,restrictTo(["admin",]),deleteWorkspace)
-router.route('/delete-project').delete(authentication,ensureWorkspaceUser,restrictTo(["admin",]),deleteProject)
-router.route('/delete-task').delete(authentication,ensureWorkspaceUser,restrictTo(["admin","member"]),deleteTask)
-router.route('/delete-team-member').delete(authentication,ensureWorkspaceUser,restrictTo(["admin",]),deleteTeamMember)
-router.route('/delete-project-member').delete(authentication,ensureWorkspaceUser,restrictTo(["admin","member"]),deleteProjectMember)
-router.route('/delete-comments').delete(authentication,ensureWorkspaceUser,restrictTo(["admin",]),deleteComments)
+router.route('/create-project').post(checkSchema(createProjectSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["org:admin"]),createProject)
+router.route('/create-task').post(checkSchema(createTaskSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["org:admin","org:member"]),createTask)
+router.route('/get-my-task').post(authentication,ensureWorkspaceUser,restrictTo(["org:admin","org:member"]),getMyTasks)
+router.route('/add-workspace-member').post(checkSchema(addTeamMemberToWorkspaceSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["org:admin"]),addTeamMemberToWorkspace)
+router.route('/add-project-member').post(checkSchema(updateProjectMemberSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["org:admin","org:member"]),addMemberToProject)
+router.route('/add-comment').post(checkSchema(addCommentSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["org:admin","org:member"]),addComments)
+router.route('/get-details').get(authentication,ensureWorkspaceUser,restrictTo(['org:admin',"org:member"]),getFullWorkspaceDetails)
+router.route('/update-workspace').put(checkSchema(updateWorkspaceSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["org:admin",]),updateWorkspace)
+router.route('/update-project').put(checkSchema(updateProjectSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["org:admin","org:member"]),updateProject)
+router.route('/update-task').put(checkSchema(updateTaskSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["org:admin","org:member"]),updateTask)
+router.route('/update-project-member').put(checkSchema(updateProjectMemberSchema),schemaValidation,authentication,ensureWorkspaceUser,restrictTo(["org:admin",]),updateProjectMember)
+router.route('/delete-workspace').delete(authentication,ensureWorkspaceUser,restrictTo(["org:admin",]),deleteWorkspace)
+router.route('/delete-project').delete(authentication,ensureWorkspaceUser,restrictTo(["org:admin",]),deleteProject)
+router.route('/delete-task').delete(authentication,ensureWorkspaceUser,restrictTo(["org:admin","org:member"]),deleteTask)
+router.route('/delete-team-member').delete(authentication,ensureWorkspaceUser,restrictTo(["org:admin",]),deleteWorkspaceMember)
+router.route('/delete-project-member').delete(authentication,ensureWorkspaceUser,restrictTo(["org:admin","org:member"]),deleteProjectMember)
+router.route('/delete-comments').delete(authentication,ensureWorkspaceUser,restrictTo(["org:admin",]),deleteComments)
 
 
 export default router;
