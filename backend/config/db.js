4c73de5df0b0b20104a8d1d053c1b70ed46994f8
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-03-09 12:09:07 +0530

    schema updated for query api

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import {Pool} from 'pg';
 import {config} from 'dotenv';
 import {drizzle} from 'drizzle-orm/node-postgres';
-import * as schema from '../models/workspace.model.js'
+import {schema} from '../models/schema.js'
 config()
 
 const pool = new Pool({
