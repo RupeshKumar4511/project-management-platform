c9226ce775be8b44411d8385417c82b9cd7f3835
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-03-09 12:10:04 +0530

    ensureAuth.js updated

---
+++ backend/middleware/ensureAuth.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import {config} from 'dotenv';
+config()
+export const ensureAuth = async(req,res,next)=>{
+    const accessToken = req.cookies?.accessToken;
+    if(!accessToken){
+        return res.status(401).send({success:false,message:"Access Token is required"})
+    }
+
+    try {
+        const decodedData = jwt.verify(accessToken,process.env.JWT_SECRET);
+        req.user = decodedData;
+        
+
+        return next()
+        
+    } catch (error) {
+        const message = error.name === 'TokenExpiredError' 
+            ? "Token expired" 
+            : "Invalid token";
+
+        return res.status(401).json({ 
+            success: false, 
+            message 
+        });
+    }
+}
