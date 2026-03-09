0c01ab13aed931aee2f18e2b0aed6d7ae791e61e
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-03-09 12:06:47 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import userRoutes from './routes/user.routes.js'
+import authRoutes from './routes/auth.routes.js'
 import workspaceRoutes from './routes/workspace.routes.js'
 
 const app = express();
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}))
 
 
-app.use('/api/v1/users',userRoutes);
+app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/workspace',workspaceRoutes);
 
 
