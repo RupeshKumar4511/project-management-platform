37970170e4cf0a6e8a0aa4fe4858b8b3ea0801b1
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-03-26 23:24:59 +0530

    app.js updated

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import authRoutes from './routes/auth.routes.js'
+import userRoutes from './routes/user.routes.js'
 import workspaceRoutes from './routes/workspace.routes.js'
 import { ensureAuth } from './middleware/ensureAuth.js';
 
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 
 app.use('/api/v1/auth',authRoutes);
+app.use('/api/v1/users/',ensureAuth,userRoutes)
 app.use('/api/v1/workspace',ensureAuth,workspaceRoutes);
 
 
