03d791e3710c819e2b5c1639b509c6a6d0d164e3
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-04-03 23:32:46 +0530

    redis client imported

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config()
 import app from './app.js';
 import pool from './config/db.js';
-
+import client from './config/client.js'
 const PORT = process.env.PORT || 3000;
 
 
