1378cc7f5f9469735fb9dce77c14bf507b991b8b
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-04-05 23:25:43 +0530

    chatRoutes included

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import authRoutes from './routes/auth.routes.js'
 import userRoutes from './routes/user.routes.js'
 import workspaceRoutes from './routes/workspace.routes.js'
+import chatRoutes from './routes/chat.routes.js'
 import { ensureAuth } from './middleware/ensureAuth.js';
 
 const app = express();
@@ -22,6 +23,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/users/',ensureAuth,userRoutes)
 app.use('/api/v1/workspace',ensureAuth,workspaceRoutes);
+app.use('/api/v1/chat',ensureAuth,chatRoutes)
 
 
 export default app;
