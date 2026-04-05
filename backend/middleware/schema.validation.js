d83f5e84942bf80d32166a8cd3bcf7ae3af45d92
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-04-05 23:22:12 +0530

    schema.validation.js updated

--- backend/middleware/schema.validation.js
+++ backend/middleware/schema.validation.js
@@ -3,7 +3,7 @@ import  {matchedData, validationResult}  from 'express-validator';
 export const schemaValidation = async(req,res,next)=>{
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-        return res.status(400).send({message:"bad request",error:errors.array()})
+        return res.status(400).send({message:errors.array()[0].msg,error:errors.array()})
     }
     req.body = matchedData(req,{locations:['body']})
     req.params = matchedData(req,{locations:['params']})
