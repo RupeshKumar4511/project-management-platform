fb95400f8f63b0fa129334fcb8a1a6cc00e4a8d1
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-04-17 21:14:19 +0530

    github oauth controller added

---
+++ backend/controllers/github.controller.js
@@ -0,0 +1,73 @@
+import { and, eq } from 'drizzle-orm'
+import { db } from '../config/db.js';
+import bcrypt from 'bcrypt'
+import generator from 'generate-password'
+import { oauthAccount, users } from '../models/user.model.js'
+
+
+export const getUserWithOauthId = async ({ provider, email }) => {
+
+    const [user] = await db.select({
+        id: users.id,
+        username: users.name,
+        email: users.email,
+        role : users.role,
+        providerAccountId: oauthAccount.providerAccountId,
+        provider: oauthAccount.provider,
+
+    }).from(users)
+        .where(eq(users.email, email))
+        .leftJoin(oauthAccount,
+            and(
+                eq(oauthAccount.provider, provider),
+                eq(oauthAccount.userId, users.id)
+            ))
+
+    if (!user) return null;
+
+    return user;
+}
+
+
+export const linkUserWithOauth = async ({ userId, provider, providerAccountId }) => {
+    await db.insert(oauthAccount).values({
+        userId,
+        provider,
+        providerAccountId
+    })
+}
+
+
+export const createUserWithOauth = async ({ name, email, provider, providerAccountId }) => {
+    const user = await db.transaction(async (trx) => {
+        const password = generator.generate({
+            length: 8,
+            numbers: true,
+            symbols: '@$!%*?&',
+            uppercase: true,
+            lowercase: true,
+            strict: true
+        });
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const [user] = await trx.insert(users).values({ name, email, password: hashedPassword, role:'admin' }).
+            $returningId;
+
+        await trx.insert(oauthAccount).values({
+            provider,
+            providerAccountId,
+            userId: user.id
+        })
+
+        return user;
+
+    })
+
+    return {
+        username: name,
+        email,
+        id: user.id,
+        role: 'admin'
+    }
+}
+
