0caf99ebc58265f514c17019cdd10a7991cdd247
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-04-18 21:47:29 +0530

    ensureAuth updated

--- frontend/src/features/ensureAuth.js
+++ frontend/src/features/ensureAuth.js
@@ -1,18 +1,20 @@
+import { redirect } from "react-router-dom";
+
 const generateNewRefreshToken = async () => {
-    try {
-        const response = await fetch('https://project-management-platform-d4jp.onrender.com/api/v1/auth/refresh', {
-            method: "POST",
-            headers: { "Content-Type": 'application/json' },
-            credentials:'include'
-        })
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        return error;
-    }
+  try {
+    const response = await fetch('https://project-management-platform-d4jp.onrender.com/api/v1/auth/refresh', {
+      method: "POST",
+      headers: { "Content-Type": 'application/json' },
+      credentials: 'include'
+    })
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 }
-const  ensureAuth = async () => {
-  
+const ensureAuth = async () => {
+
   const customMsg = { message: "Logout Successfully", logout: true };
 
   try {
@@ -25,25 +27,26 @@ const  ensureAuth = async () => {
     if (response.ok) {
       localStorage.setItem("user", JSON.stringify(data));
       return data;
-    } else if(data.message == 'Token expired'){
-        const res = await generateNewRefreshToken();
-        if(res.code == 'SIGNED_OUT'){
-          localStorage.setItem("user", JSON.stringify({logout:true}));
-          alert("You are Signed out. Please Login to continue");
-          window.location.href='/'
-          return data;
-        }else{
-          localStorage.setItem("user", JSON.stringify(res));
-        }   
     }
-    else {
-      localStorage.setItem("user", JSON.stringify(customMsg));
-      return customMsg;
+
+    if (data.message == 'Token expired') {
+      const res = await generateNewRefreshToken();
+      if (res.code == 'SIGNED_OUT') {
+        localStorage.setItem("user", JSON.stringify({ logout: true }));
+        return redirect('/')
+      } else {
+        localStorage.setItem("user", JSON.stringify(res));
+        return res;
+      }
     }
+
+    localStorage.setItem("user", JSON.stringify(customMsg));
+    return redirect('/');
+
   } catch (error) {
     console.log(error);
     localStorage.setItem("user", JSON.stringify(customMsg));
-    return customMsg;
+    return redirect('/');
   }
 };
 
