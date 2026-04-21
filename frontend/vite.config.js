eccdbfff4b7912d4f27b53a2b099376ad2e14113
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-04-21 23:32:45 +0530

    vite.confit updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: [import.meta.env.VITE_FRONTEND_DOMAIN_NAME],
+    allowedHosts: ['projectly-kj9b.onrender.com'],
   },
 })
