95dfbf123a2e0999f19d8f63c40d52a220f5e353
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-04-21 22:44:07 +0530

    payment/details route added

--- backend/routes/payment.routes.js
+++ backend/routes/payment.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { checkout, paymentVerification } from '../controllers/payment.controller.js';
+import { checkout, getUserPlanDetails, paymentVerification } from '../controllers/payment.controller.js';
 import {checkSchema} from 'express-validator'
 import { checkoutSchema, paymentVerificationSchema } from '../utils/payment.validationSchema.js';
 import { schemaValidation } from '../middleware/schema.validation.js';
@@ -8,7 +8,7 @@ const router = Router();
 
 router.route('/').post(checkSchema(checkoutSchema),schemaValidation,checkout);
 router.route('/verification').post(checkSchema(paymentVerificationSchema),schemaValidation,paymentVerification)
-
+router.route('/details').get(getUserPlanDetails)
 
 export default router;
 
