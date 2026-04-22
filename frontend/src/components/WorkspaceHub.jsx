7f2102dd52b9341b12e8dc7d5724714c8543f338
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-04-22 14:36:55 +0530

    component updated

--- frontend/src/components/WorkspaceHub.jsx
+++ frontend/src/components/WorkspaceHub.jsx
@@ -2,14 +2,11 @@ import Header from './Header.jsx'
 import Footer from './Footer.jsx'
 import AppSideBar from './AppSideBar.jsx';
 import { Outlet } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import {  useState } from 'react';
 
 export default function WorkspaceHub() {
   const [ open, setOpen ] = useState(false);
 
-  useEffect(()=>{
-    window.location.reload()
-  },[])
 
   return (
     <>
