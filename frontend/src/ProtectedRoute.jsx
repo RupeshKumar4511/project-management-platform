76c93ec0132c55a437010f2f11695f743dff8f87
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-04-22 13:54:18 +0530

    component updated

--- frontend/src/ProtectedRoute.jsx
+++ frontend/src/ProtectedRoute.jsx
@@ -1,29 +1,19 @@
 import {useEffect} from 'react'
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useLoaderData, useNavigate } from 'react-router-dom';
-import { authActions } from './features/authSlice';
 
 const ProtectedRoute = ({ children }) => {
     const { authResponse } = useSelector((store) => store.auth);
-    const userData = useLoaderData();
     const navigate = useNavigate();
-    const dispatch = useDispatch();
+    const userData = useLoaderData();
 
     useEffect(() => {
         
-        if (userData?.success && !authResponse.success) {
-            dispatch(authActions.setAuth(userData)); 
-        }
-
-        if (userData?.logout || (!userData?.success && !authResponse.success)) {
+        if (!userData?.success && !authResponse.success) {
             navigate('/');
         }
-    }, [userData, authResponse, navigate, dispatch]);
+    }, [authResponse, navigate]);
 
-    
-    if (!userData?.success) {
-        return null; 
-    }
 
     return children;
 };
