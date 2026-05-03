b4f945f329f7a1326edbe950233cfcff70515d83
RupeshKumar4511 <kumarrn4511@gmail.com>
2026-05-03 15:14:11 +0530

    component updated

--- frontend/src/components/AppSideBar.jsx
+++ frontend/src/components/AppSideBar.jsx
@@ -71,7 +71,7 @@ export default function AppSideBar({ open, setOpen }) {
             bottom-0 ${open ? 'flex' : 'hidden'}`}>
                 <NavLink to="/app" className="py-1 lg:text-xl" onClick={() => setOpen(!open)}>Home</NavLink>
                 <NavLink to={`/app/${URL}`} className="py-1 lg:text-xl" onClick={() => setOpen(!open)}>Workspace</NavLink>
-                <div className="py-1 flex lg:text-xl" onClick={() => setMenu((prevState) => !prevState)}>{getUserName(authResponse.username)}<IoIosArrowDropdownCircle className='relative top-2' size={15} /></div>
+                <div className="py-1 flex lg:text-xl" onClick={() => setMenu((prevState) => !prevState)}>{getUserName(authResponse?.username || "User")}<IoIosArrowDropdownCircle className='relative top-2' size={15} /></div>
                 <ul className={`px-2 ${menu ? '' : 'hidden'} md:text-md text-sm`}>
                     <li><NavLink to="/app/profile" onClick={() => setOpen(!open)}>Profile</NavLink></li>
                     <li className='cursor-pointer' onClick={() => {
